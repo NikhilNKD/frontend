c4bd38a490ec66d2e1d145fba7ea44530ec04c1e
rajanflyingarrow <113270603+rajanflyingarrow@users.noreply.github.com>
2024-06-24 20:16:25 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+varbookId = 100;
+
+functionfindBook(id){
+for(vari =0; i<books.length; i++){
+if(books[i].id === id){
+return books[i];
+}
+}
+return null;
+
+}
